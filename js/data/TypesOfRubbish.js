32a6dbbc3b015396aa25117987d1a521eb4bffea
AnioPa <pacholskanna@gmail.com>
2019-05-04 00:36:07 +0200

    Nowa tblica obiektów

--- js/data/TypesOfRubbish.js
+++ js/data/TypesOfRubbish.js
@@ -1,16 +1,171 @@
-const TypesOfRubbish= [
+module.exports=[
     {
-        nameOfBasket: 'BIO',
-        description:'odpadki organiczne'
+        name:'papier',
+        category:'papier',
+        basket:'pojemnika o kolorze niebieskim'
+    },
+    {
+        name:'gazety',
+        category:'papier',
+        basket:'pojemnika o kolorze niebieskim'
+    },
+    {
+        name:'gazeta',
+        category:'papier',
+        basket:'pojemnika o kolorze niebieskim'
+    },
+    {
+        name:'karton',
+        category:'papier',
+        basket:'pojemnika o kolorze niebieskim'
+    },
+    {
+        name:'kartony',
+        category:'papier',
+        basket:'pojemnika o kolorze niebieskim'
+    },
+    {
+        name:'ulotka',
+        category:'papier',
+        basket:'pojemnika o kolorze niebieskim'
+    },
+    {
+        name:'ulotki',
+        category:'papier',
+        basket:'pojemnika o kolorze niebieskim'
+    },
+    {
+        name:'zeszyt',
+        category:'papier',
+        basket:'pojemnika o kolorze niebieskim'
+    },
+    {
+        name:'zeszyty',
+        category:'papier',
+        basket:'pojemnika o kolorze niebieskim'
+    },
+    {
+        name:'słoiki',
+        category:'szkło',
+        basket:'pojemnika o kolorze zielonym'
 
     },
     {
-        nameOfBasket: 'Papier',
-        description: 'suche opakawnia'
-
+        name:'czyste opakowania z papieru',
+        category:'papier',
+        basket:'pojemnika o kolorze niebieskim'
     },
     {
-        nameOfBasket: 'Metale i tworzywa sztuczne',
-        description:'butelki plastikowe'
-    }
-]
+        name:'tektura',
+        category:'papier',
+        basket:'pojemnika o kolorze niebieskim'
+    },
+    {
+        name:'tworzywa sztuczne',
+        category:'metal',
+        basket:'pojemnika o kolorze zielonym'
+    },
+    {
+        name:'gazety',
+        category:'papier',
+        basket:'pojemnika o kolorze niebieskim'
+    },
+    {
+        name:'papier',
+        category:'papier',
+        basket:'pojemnika o kolorze niebieskim'
+    },
+    {
+        name:'papier',
+        category:'papier',
+        basket:'pojemnika o kolorze niebieskim'
+    },
+    {
+        name:'papier',
+        category:'papier',
+        basket:'pojemnika o kolorze niebieskim'
+    },
+    {
+        name:'papier',
+        category:'papier',
+        basket:'pojemnika o kolorze niebieskim'
+    },
+    {
+        name:'papier',
+        category:'papier',
+        basket:'pojemnika o kolorze niebieskim'
+    },
+    {
+        name:'papier',
+        category:'papier',
+        basket:'pojemnika o kolorze niebieskim'
+    },
+    {
+        name:'papier',
+        category:'papier',
+        basket:'pojemnika o kolorze niebieskim'
+    },
+    {
+        name:'papier',
+        category:'papier',
+        basket:'pojemnika o kolorze niebieskim'
+    },
+    {
+        name:'papier',
+        category:'papier',
+        basket:'pojemnika o kolorze niebieskim'
+    },
+    {
+        name:'papier',
+        category:'papier',
+        basket:'pojemnika o kolorze niebieskim'
+    },
+    {
+        name:'papier',
+        category:'papier',
+        basket:'pojemnika o kolorze niebieskim'
+    },
+    {
+        name:'papier',
+        category:'papier',
+        basket:'pojemnika o kolorze niebieskim'
+    },
+    {
+        name:'papier',
+        category:'papier',
+        basket:'pojemnika o kolorze niebieskim'
+    },
+    {
+        name:'papier',
+        category:'papier',
+        basket:'pojemnika o kolorze niebieskim'
+    },
+    {
+        name:'papier',
+        category:'papier',
+        basket:'pojemnika o kolorze niebieskim'
+    },
+    {
+        name:'papier',
+        category:'papier',
+        basket:'pojemnika o kolorze niebieskim'
+    },
+    {
+        name:'papier',
+        category:'papier',
+        basket:'pojemnika o kolorze niebieskim'
+    },
+    {
+        name:'papier',
+        category:'papier',
+        basket:'pojemnika o kolorze niebieskim'
+    },
+    {
+        name:'papier',
+        category:'papier',
+        basket:'pojemnika o kolorze niebieskim'
+    },
+
+
+
+];
