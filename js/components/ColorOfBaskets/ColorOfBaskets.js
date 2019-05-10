000ba229dde4d63fba4f59df390ed8274eaca5c9
AnioPa <pacholskanna@gmail.com>
2019-05-10 12:10:31 +0200

    .

--- js/components/ColorOfBaskets/ColorOfBaskets.js
+++ js/components/ColorOfBaskets/ColorOfBaskets.js
@@ -1,35 +1,125 @@
 import React from 'react';
-require ('./ColorOFBaskets.css');
+
+require('./ColorOFBaskets.css');
+import papier from "../../../images/papier.png";
+import bio from "../../../images/bio.png";
+import metale_i_tworzywasztuczne from "../../../images/metale_i_tworzywasztuczne.png";
+import odpady_wielkogabarytowe from "../../../images/odpady_wielkogabarytowe.png";
+import szklo from "../../../images/szklo.png";
+import zielone from "../../../images/zielone.png";
+import zmieszane from "../../../images/zmieszane.png";
 
 class ColorOfBaskets extends React.Component {
     constructor(props) {
         super(props);
-        this.state={
-            papier:''
+        this.state = {
+            papier: '',
+            bio: '',
+            zmieszane: '',
+            szklo: '',
+            metale: '',
+            zielone: '',
+            wielkgabarytowe: ''
         }
     }
-    handleChange=()=>{
-        this.setState({
-            papier:'nie wyrzucamy...'
-        })
-    }
-    handleChangeOn=()=>{
-        this.setState({
-            papier:''
-        })
-    }
 
+    handleChange = () => {
+        this.setState({
+            papier: 'nie wyrzucamy...'
+        })
+    };
+    handleChangeOn = () => {
+        this.setState({
+            papier: ''
+        })
+    };
+    handleChange1 = () => {
+        this.setState({
+            bio: 'nie wyrzucamy...'
+        })
+    };
+    handleChangeOn1 = () => {
+        this.setState({
+            bio: ''
+        })
+    };
+    handleChange2 = () => {
+        this.setState({
+            zmieszane: 'nie wyrzucamy...'
+        })
+    };
+    handleChangeOn2 = () => {
+        this.setState({
+            zmieszane: ''
+        })
+    };
+    handleChange3 = () => {
+        this.setState({
+            szklo: 'nie wyrzucamy...'
+        })
+    };
+    handleChangeOn3 = () => {
+        this.setState({
+            szklo: ''
+        })
+    };
+    handleChange4 = () => {
+        this.setState({
+            metale: 'nie wyrzucamy...'
+        })
+    };
+    handleChangeOn4 = () => {
+        this.setState({
+            metale: ''
+        })
+    };
+    handleChange5 = () => {
+        this.setState({
+            zielone: 'nie wyrzucamy...'
+        })
+    };
+    handleChangeOn5 = () => {
+        this.setState({
+            zielone: ''
+        })
+    };
+    handleChange6 = () => {
+        this.setState({
+            wielkgabarytowe: 'nie wyrzucamy...'
+        })
+    };
+    handleChangeOn6 = () => {
+        this.setState({
+            wielkgabarytowe: ''
+        })
+
+    };
 
 
     render() {
         return <div className='colorOfBaskets'>
-            <div onMouseEnter={this.handleChange} onMouseLeave={this.handleChangeOn} className='papier' >{this.state.papier}</div>
-            <div className='bio' ></div>
-            <div className='zmieszane'></div>
-            <div className='szklo' ></div>
-            <div className='metale' ></div>
-            <div className='zielone' ></div>
-            <div className='wielkogabarytowe'></div>
+            <div onMouseEnter={this.handleChange} onMouseLeave={this.handleChangeOn}
+                 className='papier'>{this.state.papier}<p>Papier</p>
+                <img src={papier}/></div>
+            <div onMouseEnter={this.handleChange1} onMouseLeave={this.handleChangeOn1} className='bio'>{this.state.bio}
+                <p>bio</p>
+                <img src={bio}/>
+            </div>
+            <div onMouseEnter={this.handleChange2} onMouseLeave={this.handleChangeOn2}
+                 className='zmieszane'>{this.state.zmieszane}<p>zmieszane</p>
+                <img src={zmieszane}/></div>
+            <div onMouseEnter={this.handleChange3} onMouseLeave={this.handleChangeOn3}
+                 className='szklo'>{this.state.szklo}<p>szkło</p>
+                <img src={szklo}/></div>
+            <div onMouseEnter={this.handleChange4} onMouseLeave={this.handleChangeOn4}
+                 className='metale'>{this.state.metale}<p>metale i tworzywa sztuczne</p>
+                <img src={metale_i_tworzywasztuczne}/></div>
+            <div onMouseEnter={this.handleChange5} onMouseLeave={this.handleChangeOn5}
+                 className='zielone'>{this.state.zielone}<p>zielone</p>
+                <img src={zielone}/></div>
+            <div onMouseEnter={this.handleChange6} onMouseLeave={this.handleChangeOn6}
+                 className='wielkogabarytowe'>{this.state.wielkgabarytowe}<p>odpady wielkogabarytowe</p>
+                <img src={odpady_wielkogabarytowe}/></div>
         </div>
 
     }
--- js/components/SearchBar/TrashList/TrashList.js
+++ js/components/SearchBar/TrashList/TrashList.js
@@ -9,17 +9,15 @@ class TrashList extends React.Component {
     }
     render() {
         const trash=this.props.trash.map((element,index)=>
-        { return <div className=''>
-                <img src={element.image} alt=''/>
-                <div className='info'>
-                    <li className='TrashListLi'key={element.name}>{element.name} (należy/ą do kategorii {element.category}) wyrzucamy do  {element.basket} <br/> {element.description} <div className='TarshListDiv'>{element.image}</div></li>
-                </div>
+        { return <div className='TrashListDiv'>
+                <img className='TrashListImg' src={element.image} alt=''/>
+                <li className='TrashListLi' key={element.name}>{element.name} (należy/ą do kategorii {element.category}) wyrzucamy do  {element.basket} <br/> {element.description}</li>
             </div>
 
         });
         if(this.props.trash.length>0){
             return(
-                <ul style={{listStyle:'none'}}>
+                <ul className='TrashList' style={{listStyle:'none'}}>
                     {trash}
                 </ul>
             )
